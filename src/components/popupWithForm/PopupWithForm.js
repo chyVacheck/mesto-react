0e14954c2152ea0e3f8c45d225e453e16370f9ba
Dmytro Shakh <work.dmytro02@gmail.com>
2022-10-09 17:06:05 +0200

    refactor: включить валидацию, убрать комент и одинарный тег

--- src/components/popupWithForm/PopupWithForm.js
+++ src/components/popupWithForm/PopupWithForm.js
@@ -3,35 +3,32 @@ function PopupWithForm({
 }) {
 
   return (
-    <>
-      {/* pop-up */}
-      <section id={`popup-${name}`} className={isOpen ? 'popup popup_opened' : 'popup'}>
-        <article id={`popup-${name}-container`} className="popup__container">
-          <h2 className="popup__title">{popupTitle}</h2>
-          <form name={`${name}-form`} id={`${name}-popup-form`} noValidate>
-            {/* all inputs */}
-            {children}
-            {/* submit */}
-            <button
-              disabled
-              type="submit"
-              id={`${name}-button-submit`}
-              className="popup__submit-button popup__submit-button_invalid"
-            >
-              {buttonTitle}
-            </button>
-          </form>
-
+    <section id={`popup-${name}`} className={isOpen ? 'popup popup_opened' : 'popup'}>
+      <article id={`popup-${name}-container`} className="popup__container">
+        <h2 className="popup__title">{popupTitle}</h2>
+        <form name={`${name}-form`} id={`${name}-popup-form`}> {/*todo noValidate */}
+          {/* all inputs */}
+          {children}
+          {/* submit */}
           <button
-            aria-label="закрыть окно pop-up"
-            type="button"
-            id={`${name}-button-close`}
-            className="popup__close-button button"
-            onClick={onClose}
-          ></button>
-        </article>
-      </section>
-    </>
+            disabled
+            type="submit"
+            id={`${name}-button-submit`}
+            className="popup__submit-button popup__submit-button_invalid"
+          >
+            {buttonTitle}
+          </button>
+        </form>
+
+        <button
+          aria-label="закрыть окно pop-up"
+          type="button"
+          id={`${name}-button-close`}
+          className="popup__close-button button"
+          onClick={onClose}
+        ></button>
+      </article>
+    </section>
 
   )
 }

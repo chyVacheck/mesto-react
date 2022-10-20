ca58fddc1cea51390fc6263209e48cbe1e800547
Dmytro Shakh <work.dmytro02@gmail.com>
2022-10-20 18:53:36 +0200

    Update component Card
    Немного переделана логика, а именно под контекст CurrentUserContext теперь в зависимости от него добавляются те или иные классы

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,8 +1,29 @@
 
+import React from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext.js";
+
+
 function Card(props) {
   const name = props.name;
   const link = props.link;
   const length = props.length;
+
+  const currentUser = React.useContext(CurrentUserContext);
+
+  const isOwn = props.card.owner._id === currentUser._id;
+
+  //? Создаём переменную, которую после зададим в `className` для кнопки удаления
+  const cardDeleteButtonClassName = (
+    `elements__card-trash button ${isOwn ? 'elements__card-trash_visible' : 'elements__card-trash_hidden'}`
+  );
+
+  //? Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+  const isLiked = props.card.likes.some((i) => i._id === currentUser._id)
+
+  //? Создаём переменную, которую после зададим в `className` для кнопки лайка
+  const cardLikeButtonClassName = `elements__card-like button ${isLiked ? 'elements__card-like_active' : ''
+    }`;
+
 
   function handleClick() {
     props.onCardClick(props.card);
@@ -15,7 +36,7 @@ function Card(props) {
         id="button-trash"
         aria-label="удалить карточку"
         type="button"
-        className="elements__card-trash button"
+        className={`${cardDeleteButtonClassName}`}
       />
       <button className="elements__card-image-button" onClick={handleClick}>
         <img className="elements__card-image" alt={name} src={link} />
@@ -27,7 +48,7 @@ function Card(props) {
             id="button-like"
             aria-label="поставить или убрать лайк"
             type="button"
-            className="elements__card-like button"
+            className={`${cardLikeButtonClassName}`}
           />
           <p className="elements__card-like-number">{length}</p>
         </div>

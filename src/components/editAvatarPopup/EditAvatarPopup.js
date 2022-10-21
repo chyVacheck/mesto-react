78970f713c55f18061b274e2ba87fea7bc2dfd81
Dmytro Shakh <work.dmytro02@gmail.com>
2022-10-21 19:51:11 +0200

    Create EditAvatarPopup.js

---
+++ src/components/editAvatarPopup/EditAvatarPopup.js
@@ -0,0 +1,55 @@
+
+import React from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import PopupWithForm from "../popupWithForm/PopupWithForm";
+import Input from '../input/Input.js';
+
+function EditAvatarPopup(props) {
+
+  const isEditAvatarPopupOpen = props.isOpen;
+  const closeAllPopups = props.onClose;
+
+  const [avatar, setAvatar] = React.useState();
+
+  //? Подписка на контекст
+  const currentUser = React.useContext(CurrentUserContext);
+
+  //? После загрузки текущего пользователя из API
+  //? его данные будут использованы в управляемых компонентах.
+  React.useEffect(() => {
+    setAvatar('');
+  }, [currentUser, isEditAvatarPopupOpen]);
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    props.onUpdateAvatar(avatar);
+  }
+
+  return (
+    <PopupWithForm
+      onSubmit={handleSubmit}
+      name='avatar'
+      popupTitle='Обновить аватар'
+      buttonTitle='Сохранить'
+      isOpen={isEditAvatarPopupOpen}
+      onClose={closeAllPopups}
+    >
+      {/* name */}
+      <Input
+        isOpen={isEditAvatarPopupOpen}
+        setValue={setAvatar}
+        name={'avatar'}
+        type={'url'}
+        value={avatar}
+        minLength={null} //? в данном случае не нужны
+        maxLength={null} //? в данном случае не нужны
+        required={true}
+        placeholder={'Ссылка на картинку'}
+        id_name_popup={'avatar'}
+        id_name={'info'}
+      />
+    </PopupWithForm >
+  )
+}
+
+export default EditAvatarPopup;

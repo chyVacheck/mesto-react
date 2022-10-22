a35ad5b3461737279d56b8632d6de0ac1b7d02a2
Dmytro Shakh <work.dmytro02@gmail.com>
2022-10-22 14:33:22 +0200

    style

--- src/components/imagePopup/ImagePopup.js
+++ src/components/imagePopup/ImagePopup.js
@@ -4,29 +4,26 @@ function ImagePopup(props) {
   const isOpen = props.isOpen;
 
   return (
-    <>
-      {/* <!-- * card pop-up --> */}
-      <section id="popup-card" className={isOpen ? 'popup popup_background_darknes popup_opened' : 'popup popup_background_darknes'} >
-        <article id="popup-card-container" className="popup__card">
-          <img
-            className="popup__card-image"
-            alt={selectedCard === null ? null : selectedCard.name}
-            src={selectedCard === null ? null : selectedCard.link}
+    <section id="popup-card" className={isOpen ? 'popup popup_background_darknes popup_opened' : 'popup popup_background_darknes'} >
+      <article id="popup-card-container" className="popup__card">
+        <img
+          className="popup__card-image"
+          alt={selectedCard === null ? null : selectedCard.name}
+          src={selectedCard === null ? null : selectedCard.link}
 
-          />
-          <h2 className="popup__card-title">
-            {selectedCard === null ? null : selectedCard.name}
-          </h2>
-          <button
-            onClick={closeAllPopups}
-            aria-label="закрыть окно pop-up"
-            type="button"
-            id="card-button-close"
-            className="popup__close-button button"
-          />
-        </article>
-      </section>
-    </>
+        />
+        <h2 className="popup__card-title">
+          {selectedCard === null ? null : selectedCard.name}
+        </h2>
+        <button
+          onClick={closeAllPopups}
+          aria-label="закрыть окно pop-up"
+          type="button"
+          id="card-button-close"
+          className="popup__close-button button"
+        />
+      </article>
+    </section >
   )
 }
 

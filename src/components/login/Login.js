d9612987589b5c494182260b7f2f89b76086b3a5
Dmytro Shakh <work.dmytro02@gmail.com>
2022-11-06 12:35:47 +0100

    Update Login.js

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,11 +1,12 @@
 
 import SingForm from '../signForm/SignForm.js';
 
-function Login() {
+function Login({ handleAuthorization }) {
   return (
     <section className="sign">
       <article className="sign__container">
         <SingForm
+          onSubmit={handleAuthorization}
           formTitle={'Вход'}
           submitTitle={'Войти'}
           id={'login'}

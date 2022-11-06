3767657f281f7a2ab70e71f857e54f0b17d80c32
Dmytro Shakh <work.dmytro02@gmail.com>
2022-11-06 12:35:42 +0100

    Update Register.js

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,16 +1,19 @@
 import SingForm from '../signForm/SignForm.js';
+import { NavLink } from 'react-router-dom';
 
-function Register() {
+function Register({ handleRegistration }) {
+
   return (
     <section className="sign">
       <article className="sign__container">
         <SingForm
+          onSubmit={handleRegistration}
           formTitle={'Регистрация'}
           submitTitle={'Зарегистрироваться'}
           id={'register'}
         />
         <h3 className="sign__already-registered">
-          Уже зарегистрированы ? <a className='link' href='/sign-in'>Войти</a>
+          Уже зарегистрированы ? <NavLink className='link' to='/signin'>Войти</NavLink>
         </h3>
       </article>
     </section>
